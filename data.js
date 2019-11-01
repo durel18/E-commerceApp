8d927eeda7c55f70918715ec3f7dc45e23974326
durel18 <dureltankoua@yahoo.fr>
2019-11-01 17:33:32 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Iphone 11 pro",
+        img: "img/download (1).JPG",
+        price: 780000,
+        company: "APPLE",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 2,
+        title: "Samsung S10+",
+        img: "img/download (2).JPG",
+        price: 658000,
+        company: "SAMSUNG",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 3,
+        title: "LG G8",
+        img: "img/download (3).JPG",
+        price: 350000,
+        company: "LG",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 4,
+        title: "Sony xz10",
+        img: "img/download (4).JPG",
+        price: 3000000,
+        company: "SONY",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 5,
+        title: "ZTE",
+        img: "img/download (5).JPG",
+        price: 90000,
+        company: "ZTE",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 6,
+        title: "ASUS",
+        img: "img/download (6).JPG",
+        price: 280000,
+        company: "ASUS",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 7,
+        title: "Tecno Phantom 9",
+        img: "img/download (7).JPG",
+        price: 194000,
+        company: "TECNO",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },
+    {
+        id: 8,
+        title: "Infinix",
+        img: "img/download (8).JPG",
+        price: 210000,
+        company: "INFINIX",
+        info:
+        "Lorem ipsum dolor amet offal butcher quinoa",
+        inCart : false,
+        count : 0,
+        total : 0
+    },    
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Iphone 11 pro",
+    img: "img/download (1).JPG",
+    price: 780000,
+    company: "APPLE",
+    info:
+    "Lorem ipsum dolor amet offal butcher quinoa",
+    inCart : false,
+    count : 0,
+    total : 0
+};
